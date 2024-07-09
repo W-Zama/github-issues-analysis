d3232127ab383d7adb6646718bdc7f1645974fef
W-Zama <wzama4667@gmail.com>
2024-07-10 03:04:35 +0900

    update to get all issues with pagination

--- get_issues.py
+++ get_issues.py
@@ -10,30 +10,46 @@ repo = os.getenv('REPO')
 bug_label = os.getenv('BUG_LABEL')
 access_token = os.getenv('ACCESS_TOKEN')
 
-# set url, headers and params
-url = f'https://api.github.com/repos/{owner}/{repo}/issues?labels={bug_label}&per_page=100'
-headers = {
-    'Authorization': f'token {access_token}',
-}
-params = {
-    'labels': bug_label,
-    'per_page': 100,
-}
+# set page information
+page = 1
+per_page = 100        # this is max value
+response_json_list = []
 
-# get issues via github api
-response = requests.get(url, headers=headers, params=params)
+while True:
+    print(
+        f'Now getting {page} page issues... (Each page contains {per_page} issues.)')
+    # set url, headers and params
+    url = f'https://api.github.com/repos/{owner}/{repo}/issues?labels={bug_label}&per_page=100'
+    headers = {
+        'Authorization': f'token {access_token}',
+    }
+    params = {
+        'labels': bug_label,
+        'page': page,
+        'per_page': per_page,
+        'state': 'all'
+    }
 
-# print(response.request.url)
-# print(response.headers.get('x-ratelimit-limit'))
-# print(response.headers.get('x-ratelimit-remaining'))
-# print(response.headers.get('x-ratelimit-reset'))
+    # get issues via github api
+    response = requests.get(url, headers=headers, params=params)
 
-with open('./output/response.txt', mode='w') as f:
+    # check response status code
     if response.status_code == 200:
-        print('OK')
-        json_string = response.text
-        json_data = json.loads(json_string)
-        pretty_json_string = json.dumps(json_data, indent=4)
-        f.write(pretty_json_string)
+        if not response.json():         # this means that response list is empty
+            break
+        response_json_list += response.json()
+        page += 1
     else:
         print(f"Failed to retrieve issues: {response.status_code}")
+        break
+
+# show github api limit information
+print('### Github API limit information ###')
+print(f'x-ratelimit-limit: {response.headers.get("x-ratelimit-limit")}')
+print(f'x-ratelimit-remaining: {response.headers.get("x-ratelimit-remaining")}')
+print(f'x-ratelimit-reset: {response.headers.get("x-ratelimit-reset")}')
+
+# write json to file
+with open('./output/response.txt', mode='w') as f:
+    pretty_json_string = json.dumps(response_json_list, indent=4)
+    f.write(pretty_json_string)
--- parse_response.py
+++ parse_response.py
@@ -7,6 +7,6 @@ json_data = json.loads(json_text)
 
 # generate csv file about bug detection time
 with open('./output/time_data.csv', 'w') as f:
-    f.write('created_at, updated_at\n')
+    f.write('created_at, updated_at, closed_at\n')
     for d in json_data:
-        f.write(f'{d["created_at"]}, {d["updated_at"]}' + '\n')
+        f.write(f'{d["created_at"]}, {d["updated_at"]}, {d["closed_at"]}' + '\n')
